3b7770cecacd430fe1de290e26bae8a4290a0fa0
WindsorElliot <elliot.cunningham@outlook.com>
2026-03-07 00:23:13 +0100

    feat: water and text component

---
+++ lib/game/components/environment/water_component.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'package:game_jam/game/my_game.dart';
+
+class WaterComponent extends RectangleComponent
+    with HasGameReference<MyGame> {
+
+  WaterComponent({required Vector2 position, required Vector2 size})
+      : super(
+          position: position,
+          size: size,
+          priority: 0,
+          paint: Paint()..color = Colors.blue,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    add(RectangleHitbox(size: size));
+  }
+}
