733b1929245ad7f472991bf015ebadef31978116
Elliot Cunningham <elliot.cunningham@outlook.com>
2026-03-07 19:28:54 +0100

    feat/flying-component-by-toto (#26)
    * chore: move enemies in component
    
    * feat: add fly component

--- lib/game/world/level_1.dart
+++ lib/game/world/level_1.dart
@@ -1,9 +1,9 @@
 import 'package:flame/components.dart';
 import 'package:game_jam/core/config/game_config.dart';
+import 'package:game_jam/game/components/enemies/simple_oponenet.dart';
 import 'package:game_jam/game/components/environment/ground_component.dart';
 import 'package:game_jam/game/components/environment/water_component.dart';
 import 'package:game_jam/game/components/environment/water_lily_component.dart';
-import 'package:game_jam/game/enemies/simple_oponenet.dart';
 
 class Level1 extends Component {
   @override
@@ -19,10 +19,16 @@ class Level1 extends Component {
       size: Vector2(100, 200),
     );
 
-    final waterLily = WaterLilyComponent(position: Vector2(800, 800), radius: 40);
+    final waterLily = WaterLilyComponent(
+      position: Vector2(800, 800),
+      radius: 40,
+    );
 
-    final simpleOpponent = SimpleOpponent(position: Vector2(100, 100), radius: 40);
-    
+    final simpleOpponent = SimpleOpponent(
+      position: Vector2(100, 100),
+      radius: 40,
+    );
+
     await addAll([ground, water, waterLily, simpleOpponent]);
   }
 }
