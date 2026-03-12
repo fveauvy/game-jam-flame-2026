30dde3755ba71d9c3f18ccf162d72395be49b088
Shinbly <jasonfachan@live.fr>
2026-03-12 21:45:36 +0100

    Enhance egg collection mechanics and gameplay tuning parameters

--- lib/game/components/allies/egg_component.dart
+++ lib/game/components/allies/egg_component.dart
@@ -7,18 +7,29 @@ import 'package:game_jam/game/my_game.dart';
 class EggComponent extends SpriteComponent with HasGameReference<MyGame> {
   final bool isInSafeHouse;
 
+  /// remove colision hitbox if true, used for the egg onThe frog's back
+  final bool isOnBack;
+
   EggComponent({
     required super.position,
     super.size,
     required this.isInSafeHouse,
-  }) : super(sprite: Sprite(Flame.images.fromCache('eggs.png')));
+
+    this.isOnBack = false,
+  }) : super(
+         sprite: Sprite(Flame.images.fromCache('eggs.png')),
+         anchor: Anchor.center,
+       );
 
   @override
   Future<void> onLoad() async {
-    add(CircleHitbox(radius: size.x / 2));
+    if (!isOnBack) {
+      add(CircleHitbox(radius: size.x / 2));
+    }
   }
 
   Future<void> collect() async {
+    if (isOnBack) return;
     if (isInSafeHouse) return;
     game.gameState.savedEggs += 1;
     removeFromParent();
