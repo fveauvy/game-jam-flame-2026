b59507587034b4a85003637f1c8a7fc61e66f2b4
WindsorElliot <elliot.cunningham@outlook.com>
2026-03-14 17:06:32 +0100

    fix: remove tled element

--- assets/tiles/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="water" tilewidth="100" tileheight="100" tilecount="1" columns="1">
- <image source="../images/water_texture.png" width="100" height="100"/>
-</tileset>
