15d9d14aee7c1c8c52b79a6e4b434cfaf3eff7ec
François VEAUVY <veauvy.fr@gmail.com>
2026-03-15 16:02:50 +0100

    perf: cache player ref for bird updates

--- lib/game/world/world_root.dart
+++ lib/game/world/world_root.dart
@@ -4,6 +4,8 @@ import 'package:game_jam/game/components/player/player_component.dart';
 class WorldRoot extends World with HasCollisionDetection {
   PlayerComponent? _player;
 
+  PlayerComponent? get player => _player;
+
   void bindPlayer(PlayerComponent player) {
     _player = player;
   }
